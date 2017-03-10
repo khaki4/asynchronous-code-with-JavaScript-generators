0ef9fbea2393acf03ccc17f8d8ee95722a3f7dc6
정지원 <4dakto@gmail.com>
2017-03-10 13:36:31 +0900

    Jiwon :: iterate over generator

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function* createHello() {
-    try {
-        const word = yield;
-        console.log(`Hello ${word}`);
-    } catch (err) {
-        console.log('ERROR', err);
-    }
+function* createCounter() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
 }
 
-const hello = createHello();
-hello.next();
-hello.throw('Something went wrong.');
+const counter = createCounter();
+// The for-of loop pauses and resumes the generator automatically, and passes us back the values that the generator yields.
+for (let i of counter) {
+    console.log(i)
+}
