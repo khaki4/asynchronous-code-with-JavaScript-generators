5bc96f2fd6a5a1b4271cd0fba33e62961004e70f
정지원 <4dakto@gmail.com>
2017-03-16 12:35:36 +0900

    Jiwon :: Add coroutine

---
+++ coroutine.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+const co = require('co');
+
+function* createQuoteFetcher() {
+  const response = yield fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+  const quote = yield response.json()
+  return `${quote.quoteText} —${quote.quoteAuthor}`
+}
+
+// replace by Module co
+// const coroutine = (gen) => {
+//   const generator = gen()
+
+//   const handle = (result) => {
+//     if (result.done) return Promise.resolve(result.value)
+//     return Promise.resolve(result.value)
+//       .then(res => handle(generator.next(res)))
+// 	}
+
+//   return handle(generator.next())
+// }
+
+const quoteFetcher = co(createQuoteFetcher)
+quoteFetcher.then(quote => console.log(quote))
+
